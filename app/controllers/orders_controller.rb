cbe2823e4f1729c6234b84d77fcf37c39e6bd5b3
Jktoo <justinktoo98@gmail.com>
2024-12-07 11:30:40 -0500

    Added function to cancel orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,6 +18,16 @@ class OrdersController < ApplicationController
     end
   end
 
+  def destroy
+    @orders = params[:param_to_del]
+    @del = Order.find_by(id: @orders)
+    @del.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to home_order_path, status: :see_other, notice: "Order was successfully canclled." }
+      format.json { head :no_content }
+    end
+  end
 
   def renderReq
   end
